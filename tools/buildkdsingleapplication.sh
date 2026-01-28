348f8568ced15ed5e3e06657c81aa69888a4dad2
qblk <qblk@users.noreply.github.com>
2026-01-28 22:59:05 +0000

    build: update kdsingleapplication to 1.2.0

--- tools/buildkdsingleapplication.sh
+++ tools/buildkdsingleapplication.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -exo pipefail
 
-FILENAME=kdsingleapplication-1.1.0
+FILENAME=kdsingleapplication-1.2.0
 ARCHIVE=$FILENAME.tar.gz
-DIRNAME=$FILENAME
+DIRNAME=KDSingleApplication-1.2.0
 
 mkdir -p build && cd build
 
 if [ ! -d $DIRNAME ]; then
-    curl -s -L -o $ARCHIVE https://github.com/KDAB/KDSingleApplication/releases/download/v1.1.0/$ARCHIVE
+    curl -s -L -o $ARCHIVE https://github.com/KDAB/KDSingleApplication/releases/download/v1.2.0/$ARCHIVE
     tar zxf $ARCHIVE
 fi
 
@@ -16,7 +16,6 @@ cd $DIRNAME
 
 qt-cmake -S . -B build \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DKDSingleApplication_QT6=true \
     -DKDSingleApplication_STATIC=true \
     -DKDSingleApplication_TESTS=false \
     -DKDSingleApplication_EXAMPLES=false \
